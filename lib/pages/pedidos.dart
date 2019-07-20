3dac7256d16037f317ff1d90a4c8d3b8345b2ae8
edycarreyes99 <edycarreyes@gmail.com>
2019-07-19 22:32:05 -0600

    se intenta realizar la extraccion de los pedidos directamente desde la base de datos de firestore de una nueva manera

--- lib/pages/pedidos.dart
+++ lib/pages/pedidos.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'dart:async';
+import '../classes/Pedido.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class PedidosPage extends StatefulWidget {
   @override
@@ -7,6 +10,50 @@ class PedidosPage extends StatefulWidget {
 }
 
 class _PedidosPageState extends State<PedidosPage> {
+  List<Pedido> pedidos;
+  Firestore fs = Firestore.instance;
+  StreamSubscription<QuerySnapshot> pedidoSub;
+
+  Stream<QuerySnapshot> getListaPedidos({int offset, int limit}) {
+    Stream<QuerySnapshot> snapshots = fs.collection('Pedidos').snapshots();
+
+    if (offset != null) {
+      snapshots = snapshots.skip(offset);
+    }
+
+    if (limit != null) {
+      snapshots = snapshots.take(limit);
+    }
+
+    return snapshots;
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    pedidos = new List();
+
+    pedidoSub.cancel();
+
+    pedidoSub = this.getListaPedidos().listen((QuerySnapshot snapshot) {
+      final List<Pedido> pedidoss = snapshot.documents
+          .map((documentSnapshot) => Pedido.fromMap(documentSnapshot.data))
+          .toList();
+
+      setState(() {
+        this.pedidos = pedidoss;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    pedidoSub.cancel();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
