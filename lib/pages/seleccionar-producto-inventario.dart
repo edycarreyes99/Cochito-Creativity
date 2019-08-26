32f454eaf2e26fa55bb29dab50e1f4ad76a395f6
Edycar Reyes <edycarreyes@gmail.com>
2019-08-25 22:35:26 -0600

    se crea el archivo que contendra la vista para seleccionar un producto para agregarlo a un pedido de un cliente

---
+++ lib/pages/seleccionar-producto-inventario.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+class SeleccionarProductoInventarioPage extends StatefulWidget {
+  @override
+  _SeleccionarProductoInventarioPageState createState() =>
+      _SeleccionarProductoInventarioPageState();
+}
+
+class _SeleccionarProductoInventarioPageState
+    extends State<SeleccionarProductoInventarioPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Hola Mundo'),
+      ),
+    );
+  }
+}
