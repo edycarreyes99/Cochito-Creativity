9e6e0b39f2c6131cf065be8d3ea0981995744e52
Edycar Reyes <edycarreyes@gmail.com>
2020-05-17 18:48:21 -0600

    Configuracion para el inicio de sesion con facebook completado para la plataforma android

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,22 +1,10 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-<<<<<<< HEAD
 export "FLUTTER_ROOT=C:\Users\edyca\development\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\edyca\Desktop\Android Studio Projects\Lovelia-Creativity"
+export "FLUTTER_APPLICATION_PATH=C:\Users\edyca\AndroidStudioProjects\Cochito-Creativity"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build\ios"
 export "FLUTTER_FRAMEWORK_DIR=C:\Users\edyca\development\flutter\bin\cache\artifacts\engine\ios"
-export "FLUTTER_BUILD_NAME=8.0.0"
-export "FLUTTER_BUILD_NUMBER=8"
-=======
-export "FLUTTER_ROOT=/Users/edycarreyes99/development/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/edycarreyes99/Android-Studio-Projects/Lovelia-Creativity"
-export "FLUTTER_TARGET=/Users/edycarreyes99/Android-Studio-Projects/Lovelia-Creativity/lib/main.dart"
-export "FLUTTER_BUILD_DIR=build"
-export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/Users/edycarreyes99/development/flutter/bin/cache/artifacts/engine/ios"
-export "FLUTTER_BUILD_NAME=8.0.0"
-export "FLUTTER_BUILD_NUMBER=8"
-export "TRACK_WIDGET_CREATION=true"
->>>>>>> feauture/fix-product-number-incrementor
+export "FLUTTER_BUILD_NAME=9.0.0"
+export "FLUTTER_BUILD_NUMBER=9"
